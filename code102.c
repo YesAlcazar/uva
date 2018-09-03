76fc899f99f7aa7bb1cc6e36fdd0c05f48f48a12
Éder Alcazar <ederalcazar@gmail.com>
2018-09-03 00:51:18 -0300

    Feito alguns exercícios

---
+++ code102.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+enum bottles {brown,green,clear};
+char bootle_output[] = {'B','G','C'};
+
+typedef struct _LinkedBottle {
+    _LinkedBottle 
+} LinkedBottle;
+
+int main(int argc, char const *argv[]) {
+    int line[3][3], i=0;
+    while (scanf("%d %d %d %d %d %d %d %d %d", &array[0][brown], &array[0][green], &array[0][clear], &array[1][brown], &array[1][green], &array[1][clear], &array[2][brown], &array[2][green], &array[2][clear])) {
+
+    }
+    return 0;
+}
